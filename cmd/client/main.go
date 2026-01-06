db465b0f27f758eb8f30c9f6a1e9303d6582f0a1
svlaer <seppevan@gmail.com>
2026-01-06 19:37:34 +0100

    Declare new queue from code

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
+	const rabbitConnString string = "amqp://guest:guest@localhost:5672"
+
 	fmt.Println("Starting Peril client...")
+
+	conn, err := amqp.Dial(rabbitConnString)
+	if err != nil {
+		log.Fatalf("RabbitMQ failed to connect: %v", err)
+	}
+	defer conn.Close()
+
+	fmt.Println("Peril client connected to RabbitMQ.")
+
+	username, err := gamelogic.ClientWelcome()
+	if err != nil {
+		log.Fatalf("%v", err)
+	}
+
+	_, rabbitQueue, err := pubsub.DeclareAndBind(
+		conn,
+		routing.ExchangePerilDirect,
+		fmt.Sprintf("%s.%s", routing.PauseKey, username),
+		routing.PauseKey,
+		pubsub.Transient,
+	)
+	if err != nil {
+		log.Fatalf("Failed to subscribe to pause: %v", err)
+	}
+
+	fmt.Printf("Queue %v declared and bound!\n", rabbitQueue.Name)
+
+	// Wait for ctrl+c
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+
+	fmt.Println("RabbitMQ connection closed.")
 }
--- internal/pubsub/pubsub.go
+++ internal/pubsub/pubsub.go
@@ -8,6 +8,22 @@ import (
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
+type SimpleQueueType int
+
+const (
+	Durable SimpleQueueType = iota
+	Transient
+)
+
+var queueTypeName = map[SimpleQueueType]string{
+	Durable:   "durable",
+	Transient: "transient",
+}
+
+func (sqt SimpleQueueType) String() string {
+	return queueTypeName[sqt]
+}
+
 func PublishJSON[T any](ch *amqp.Channel, exchange, key string, val T) error {
 	valJson, err := json.Marshal(val)
 	if err != nil {
@@ -15,4 +31,42 @@ func PublishJSON[T any](ch *amqp.Channel, exchange, key string, val T) error {
 	}
 
 	return ch.PublishWithContext(context.Background(), exchange, key, false, false, amqp.Publishing{ContentType: "application/json", Body: valJson})
+}
+
+func DeclareAndBind(
+	conn *amqp.Connection,
+	exchange,
+	queueName,
+	key string,
+	queueType SimpleQueueType,
+) (*amqp.Channel, amqp.Queue, error) {
+	rabbitChan, err := conn.Channel()
+	if err != nil {
+		return nil, amqp.Queue{}, fmt.Errorf("Failed to create RabbitMQ channel: %v", err)
+	}
+
+	var durable, autoDelete, exclusive bool
+	switch queueType {
+	case Durable:
+		durable = true
+		autoDelete = false
+		exclusive = false
+	case Transient:
+		durable = false
+		autoDelete = true
+		exclusive = true
+	default:
+		return nil, amqp.Queue{}, fmt.Errorf("Unknown SimpleQueueType: %s", queueType)
+	}
+
+	rabbitQueue, err := rabbitChan.QueueDeclare(queueName, durable, autoDelete, exclusive, false, nil)
+	if err != nil {
+		return nil, amqp.Queue{}, fmt.Errorf("Failed to declare queue: %v", err)
+	}
+
+	if err = rabbitChan.QueueBind(rabbitQueue.Name, key, exchange, false, nil); err != nil {
+		return nil, amqp.Queue{}, fmt.Errorf("Failed to bind exchange to queue: %v", err)
+	}
+
+	return rabbitChan, rabbitQueue, nil
 }
