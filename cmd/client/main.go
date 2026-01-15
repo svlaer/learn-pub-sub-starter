5d0810b183b9296c448fe95e28cb6010f39dce52
svlaer <seppevan@gmail.com>
2026-01-15 19:50:26 +0100

    Publish and consume moves with routing patterns

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -19,6 +19,13 @@ func handlerPause(gs *gamelogic.GameState) func(routing.PlayingState) {
 	}
 }
 
+func handlerMove(gs *gamelogic.GameState) func(gamelogic.ArmyMove) {
+	return func(move gamelogic.ArmyMove) {
+		defer fmt.Print("> ")
+		gs.HandleMove(move)
+	}
+}
+
 func main() {
 	const rabbitConnString string = "amqp://guest:guest@localhost:5672"
 
@@ -31,6 +38,11 @@ func main() {
 	defer conn.Close()
 
 	fmt.Println("Peril client connected to RabbitMQ.")
+
+	rabbitChan, err := conn.Channel()
+	if err != nil {
+		log.Fatalf("Failed to open RabbitMQ channel: %v", err)
+	}
 
 	username, err := gamelogic.ClientWelcome()
 	if err != nil {
@@ -49,8 +61,19 @@ func main() {
 	); err != nil {
 		log.Fatalf("Failed to subscribe to pause: %v", err)
 	}
-
 	fmt.Println("Subscribed to pause.")
+
+	if err = pubsub.SubscribeJSON(
+		conn,
+		routing.ExchangePerilTopic,
+		fmt.Sprintf("%s.%s", routing.ArmyMovesPrefix, gs.GetUsername()),
+		fmt.Sprintf("%s.*", routing.ArmyMovesPrefix),
+		pubsub.Transient,
+		handlerMove(gs),
+	); err != nil {
+		log.Fatalf("Failed to subscribe to move queue: %v", err)
+	}
+	fmt.Println("Subscribed to army moves.")
 
 	for {
 		inputWords := gamelogic.GetInput()
@@ -67,11 +90,22 @@ func main() {
 			}
 		case "move":
 			fmt.Println("Moving...")
-			if _, err = gs.CommandMove(inputWords); err != nil {
+			move, err := gs.CommandMove(inputWords)
+			if err != nil {
 				log.Println(err)
 				continue
 			}
-			fmt.Println("Move successful")
+
+			if err = pubsub.PublishJSON(
+				rabbitChan,
+				routing.ExchangePerilTopic,
+				fmt.Sprintf("%s.%s", routing.ArmyMovesPrefix, move.Player.Username),
+				move,
+			); err != nil {
+				log.Println(err)
+				continue
+			}
+			fmt.Printf("Moved %v units to %s\n", len(move.Units), move.ToLocation)
 		case "status":
 			gs.CommandStatus()
 		case "help":
